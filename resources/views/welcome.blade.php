49cc2e5d4edb7253135ecaca42e08044352fe1f8
Bbambangg95 <bbambangg95@gmail.com>
2022-05-24 23:20:45 +0700

    Upload sluruh file

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <title>Laravel</title>
+    </head>
+    <body>
+        <div id="example"></div>
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
